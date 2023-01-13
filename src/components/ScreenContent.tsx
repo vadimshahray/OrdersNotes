19c9f24cedd6f65cfe21212312585a390b8474b9
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2023-01-14 02:02:44 +0700

    feat: customize `ScreenContent`

--- src/components/ScreenContent.tsx
+++ src/components/ScreenContent.tsx
@@ -1,24 +1,40 @@
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import React, { PropsWithChildren } from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
-export type ScreenContentProps = PropsWithChildren
+export type ScreenContentProps = {
+  noPadding?: boolean
+} & PropsWithChildren
 
-export const ScreenContent = ({ children }: ScreenContentProps) => {
+export const ScreenContent = ({
+  noPadding = false,
+  children,
+}: ScreenContentProps) => {
   const insets = useSafeAreaInsets()
+
+  const paddingH = noPadding ? 0 : 16
+  const paddingV = noPadding ? 0 : 12
 
   return (
     <View
       style={[
         {
-          paddingTop: insets.top,
-          paddingBottom: insets.bottom,
-          paddingLeft: insets.left,
-          paddingRight: insets.right,
+          paddingTop: insets.top + paddingV,
+          paddingBottom: insets.bottom + paddingV,
+          paddingLeft: insets.left + paddingH,
+          paddingRight: insets.right + paddingH,
         },
+        styles.view,
       ]}
     >
       {children}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+})
