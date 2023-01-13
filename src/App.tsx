0dddf3fee0afbdc33fecd98d5158b15061072949
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2023-01-14 00:05:34 +0700

    feat: add `react-native-paper`

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { MainScreen } from '@screens'
 import { NavigationContainer } from '@react-navigation/native'
+import { Provider as PaperProvider } from 'react-native-paper'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 const { Navigator, Screen } = createNativeStackNavigator()
 
 export default () => {
   return (
-    <NavigationContainer>
-      <Navigator>
-        <Screen name='MainScreen' component={MainScreen} />
-      </Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Navigator>
+          <Screen name='MainScreen' component={MainScreen} />
+        </Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   )
 }
--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Text} from 'react-native'
+import { Text } from 'react-native-paper'
 
 export const MainScreen = () => {
   return <Text>MainScreen text</Text>
