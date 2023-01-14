38833b9cb235e01f35501162deb1d8c305b06703
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2023-01-14 12:19:15 +0700

    feat: use typed navigation

--- src/hooks/useScreenTitle.ts
+++ src/hooks/useScreenTitle.ts
@@ -1,8 +1,10 @@
 import { useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 
 export const useScreenTitle = (title: string) => {
-  const { setOptions } = useNavigation()
+  const { setOptions } =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>()
 
   useEffect(() => {
     setOptions({ title })
