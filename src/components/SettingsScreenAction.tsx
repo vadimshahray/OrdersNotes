f67d7dd7f431f021f3691d7805215b937979918c
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2023-01-14 12:04:42 +0700

    feat: add `SettingsScreen`

--- src/components/SettingsScreenAction.tsx
+++ src/components/SettingsScreenAction.tsx
@@ -1,5 +1,14 @@
 import { Appbar } from 'react-native-paper'
+import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 
 export const SettingsScreenAction = () => {
-  return <Appbar.Action icon='cog-outline' onPress={() => {}} />
+  const { navigate } =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>()
+
+  const handlePress = () => {
+    navigate('SettingsScreen')
+  }
+
+  return <Appbar.Action icon='cog-outline' onPress={handlePress} />
 }
---
+++ src/screens/SettingsScreen.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native-paper'
+import { ScreenContent } from '@components'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
+export const SettingsScreen = () => {
+  return (
+    <ScreenContent>
+      <Text>SettingScreen text</Text>
+    </ScreenContent>
+  )
+}
