5e909d0da0af406ab874b37b518755fb85ad850c
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2023-01-14 12:04:29 +0700

    feat: use typed navigation

--- src/hooks/useMainScreenHeaderRight.ts
+++ src/hooks/useMainScreenHeaderRight.ts
@@ -1,9 +1,11 @@
 import { useEffect } from 'react'
 import { SettingsScreenAction } from '@components'
 import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 
 export const useMainScreenHeaderRight = () => {
-  const { setOptions } = useNavigation()
+  const { setOptions } =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>()
 
   useEffect(() => {
     setOptions({ headerRight: SettingsScreenAction })
