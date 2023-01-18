3b2146d1062da47c8966a4dc497b225d5d1fd254
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2023-01-18 21:54:05 +0700

    feat: add order.notes to `OrderItem`

--- src/screens/MainScreen/OrdersList/OrderItem.tsx
+++ src/screens/MainScreen/OrdersList/OrderItem.tsx
@@ -1,9 +1,17 @@
-import { Text, TouchableRipple } from 'react-native-paper'
+import { Text } from 'react-native-paper'
 
 export type OrderItemProps = {
   order: Order
 }
 
 export const OrderItem = ({ order }: OrderItemProps) => {
-  return <Text>{order.name}</Text>
+  return (
+    <>
+      <Text variant='titleMedium' numberOfLines={1}>
+        {order.name}
+      </Text>
+
+      <Text numberOfLines={2}>{order.notes}</Text>
+    </>
+  )
 }
