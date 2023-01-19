f987a888e8e6c518f7e0f1188eca6bd06ffd6829
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2023-01-20 02:07:42 +0700

    refactor: change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,6 @@
 module.exports = {
-  env: {
-    es6: true,
-  },
-  extends: ['airbnb', 'prettier', 'prettier/react'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-    __DEV__: 'readonly',
-  },
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['react', 'prettier'],
+  extends: ["@react-native-community", "prettier"],
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [
-      'warn',
-      {
-        extensions: ['.tsx', '.ts'],
-      },
-    ],
-    'no-console': 'off',
-    'react/prop-types': 'off',
-    'no-param-reassign': 'off',
-    'react/state-in-constructor': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'import/prefer-default-export': 'off',
-    'react/static-property-placement': 'off',
-  },
+    'prettier/prettier': 'error'
+  }
 };
