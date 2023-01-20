ca9207bba3c0502b2693aa4149408b6d8611f1f4
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2023-01-20 22:55:20 +0700

    style: configure order photos size

--- src/screens/OrderDesignerScreen/OrderForm/OrderPhotos/OrderPhotosList/OrderPhotoItem.tsx
+++ src/screens/OrderDesignerScreen/OrderForm/OrderPhotos/OrderPhotosList/OrderPhotoItem.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Image } from 'react-native'
 import { makeThemeStyles } from '@styles'
 
-export const ORDER_PHOTO_HEIGHT = 200
+export const ORDER_PHOTO_HEIGHT = 180
 
 export type OrderPhotoItemProps = {
   photoUri: string
@@ -11,12 +11,18 @@ export type OrderPhotoItemProps = {
 export const OrderPhotoItem = ({ photoUri }: OrderPhotoItemProps) => {
   const styles = useStyles()
 
-  return <Image source={{ uri: photoUri }} style={styles.photo} />
+  return (
+    <Image
+      source={{ uri: photoUri }}
+      style={styles.photo}
+      resizeMode='center'
+    />
+  )
 }
 
 const useStyles = makeThemeStyles((theme) => ({
   photo: {
-    width: 100,
+    width: (ORDER_PHOTO_HEIGHT * 3) / 4,
     height: ORDER_PHOTO_HEIGHT,
 
     borderRadius: theme.roundness,
