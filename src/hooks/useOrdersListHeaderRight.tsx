5195240a49ec1b7929daca0292ed66396a3542b2
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2023-01-20 14:39:51 +0700

    feat: hide delete all orders action if no orders

--- src/hooks/useOrdersListHeaderRight.tsx
+++ src/hooks/useOrdersListHeaderRight.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useNavigation } from '@hooks'
+import { useSelector } from 'react-redux'
+import { selectOrders } from '@selectors'
 import { DeleteAllOrdersAction } from '@components'
 
 export const useOrdersListHeaderRight = () => {
+  const ordersLength = useSelector(selectOrders).length
+
   const { setOptions } = useNavigation()
 
   useEffect(() => {
     setOptions({
-      headerRight: () => <DeleteAllOrdersAction />,
+      headerRight: () => (ordersLength ? <DeleteAllOrdersAction /> : undefined),
     })
-  }, [setOptions])
+  }, [ordersLength, setOptions])
 }
