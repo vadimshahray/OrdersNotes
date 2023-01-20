f31e503dfaf4e91ee7220719ae4faeba7f017cbd
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2023-01-20 13:51:07 +0700

    chore: delete useless component

--- src/components/SettingsScreenAction.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Appbar } from 'react-native-paper'
-
-export const SettingsScreenAction = ({ onPress }: AppBarActionProps) => {
-  return <Appbar.Action icon='cog-outline' onPress={onPress} />
-}
