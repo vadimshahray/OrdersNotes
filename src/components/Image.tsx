e0a909f3eeff693def8186e6261dd19cf72e680d
shaxraj.2021@stud.nstu.ru <shaxraj.2021@stud.nstu.ru>
2023-01-21 21:55:00 +0700

    refactor: set `Image` props override default props

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -4,9 +4,9 @@ import FastImage, { FastImageProps } from 'react-native-fast-image'
 export const Image = (props: FastImageProps) => {
   return (
     <FastImage
-      {...props}
       resizeMode='cover'
       defaultSource={require('@assets/placeholder.png')}
+      {...props}
     />
   )
 }
